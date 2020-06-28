60b7686d63979629ec73ad766c6abf606b005d3f
Shawn Lin <xiaoliang.lin@columbia.edu>
2020-06-28 14:12:51 -0400

    Add second index

--- example/returns.R
+++ example/returns.R
@@ -1,7 +1,8 @@
 library(pyformr)
 
-twitter <- ReturnSeries$read_csv("pyform/data/twitter_returns.csv")
-spy <- ReturnSeries$read_csv("pyform/data/spy_returns.csv")
+twitter <- ReturnSeries$read_csv("pyformr/data/twitter_returns.csv")
+spy <- ReturnSeries$read_csv("pyformr/data/spy_returns.csv")
+qqq <- ReturnSeries$read_csv("pyformr/data/qqq_returns.csv")
 
 print(head(twitter$to_year()))
 
@@ -10,5 +11,6 @@ print(head(twitter$to_year()))
 
 twitter$set_daterange(end="2020-12-31")
 twitter$add_benchmark(spy)
+twitter$add_benchmark(qqq)
 twitter$get_corr()
 twitter$get_corr(meta=TRUE)
