089cb74cdaefc7a5fcf6e39e37dc54afaee7eda0
Shawn Lin <xiaoliang.lin@columbia.edu>
2020-06-28 14:23:22 -0400

    Add example to initialize from R

--- example/returns.R
+++ example/returns.R
@@ -10,7 +10,10 @@ twitter$set_daterange(end="2014-12-31")
 print(head(twitter$to_year()))
 
 twitter$set_daterange(end="2020-12-31")
-twitter$add_benchmark(spy)
+twitter$add_benchmark(spy, "S&P 500 ETF")
 twitter$add_benchmark(qqq)
 twitter$get_corr()
 twitter$get_corr(meta=TRUE)
+
+twitter_df <- read.csv("pyformr/data/twitter_returns.csv")
+twitter_from_R <- ReturnSeries(twitter_df)
