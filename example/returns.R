2c43060aec082fe014b43ef1168b484310d39513
Shawn Lin <xiaoliang.lin@columbia.edu>
2020-06-29 00:03:12 -0400

    Add example for new update

--- example/returns.R
+++ example/returns.R
@@ -1,19 +1,55 @@
 library(pyformr)
 
+# Initialize demo
+
+# 1. from csv
 twitter <- ReturnSeries$read_csv("pyformr/data/twitter_returns.csv")
 spy <- ReturnSeries$read_csv("pyformr/data/spy_returns.csv")
 qqq <- ReturnSeries$read_csv("pyformr/data/qqq_returns.csv")
 
-print(head(twitter$to_year()))
+# 2. from internet
+libor <- CashSeries$read_fred_libor_1m()
 
-twitter$set_daterange(end="2014-12-31")
-print(head(twitter$to_year()))
-
-twitter$set_daterange(end="2020-12-31")
-twitter$add_benchmark(spy, "S&P 500 ETF")
-twitter$add_benchmark(qqq)
-twitter$get_corr()
-twitter$get_corr(meta=TRUE)
-
+# 2. from R dataframe
 twitter_df <- read.csv("pyformr/data/twitter_returns.csv")
 twitter_from_R <- ReturnSeries(twitter_df)
+
+# to period feature
+print(head(twitter$to_month()))
+print(head(twitter$to_year()))
+
+# subset date feature
+twitter$set_daterange(end = "2014-12-31")
+print(head(twitter$to_year()))
+
+# You can reset to other date range that's available initially
+# after you've set it to some date range
+twitter$set_daterange(start = "2015-01-01", end = "2019-12-31")
+print(head(twitter$to_year()))
+
+# reset to initial data
+twitter$reset()
+
+# Add benchmarks
+twitter$add_benchmark(spy, "S&P 500 ETF")
+twitter$add_benchmark(qqq)
+
+
+# common stats
+twitter$get_corr()
+twitter$get_corr(meta = TRUE)
+
+twitter$get_total_return(meta = TRUE)
+twitter$get_annualized_return(meta = TRUE)
+twitter$get_annualized_volatility(meta = TRUE)
+
+# sharpe ratio, cash handling
+# 1. default to 0
+twitter$get_sharpe_ratio(meta = TRUE)
+# 2. a constant rf rate
+twitter$get_sharpe_ratio(risk_free = 0.01, meta = TRUE)
+# 3. use libor
+twitter$add_risk_free(libor, "libor 1-month")
+twitter$get_sharpe_ratio(risk_free = "libor 1-month", meta = TRUE)
+spy$add_risk_free(libor, "libor 1-month")
+spy$get_sharpe_ratio(risk_free = "libor 1-month", meta = TRUE)
--- pyformr/R/onload.R
+++ pyformr/R/onload.R
@@ -1,9 +1,11 @@
 # global reference to scipy (will be initialized in .onLoad)
 pyform <- NULL
 ReturnSeries <- NULL
+CashSeries <- NULL
 
 .onLoad <- function(libname, pkgname) {
   # use superassignment to update global reference to scipy
   pyform <<- reticulate::import("pyform", delay_load = TRUE)
   ReturnSeries <<- pyform$returnseries$ReturnSeries
+  CashSeries <<- pyform$returnseries$CashSeries
 }
